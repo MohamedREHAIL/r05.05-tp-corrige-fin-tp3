64ff916243fc816eab36e0e121035792dc182ea9
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2024-02-16 22:02:47 +0100

    question 3.6

---
+++ src/app/api/rebuild-posts/route.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    const supabaseWebhookKey = process.env.SUPABASE_WEBHOOK_KEY;
+    const apiKey = req.headers['api-key'];
+
+    if (!apiKey) {
+        return res.status(401).json({ error: '"API-Key" header is missing' });
+    }
+
+    if (apiKey !== supabaseWebhookKey) {
+        return res.status(403).json({ error: 'API Key is not valid' });
+    }
+    console.log(req.headers);
+    return res.status(200).json({
+        revalidated: true,
+        date: new Date().toISOString(),
+    });
+}
