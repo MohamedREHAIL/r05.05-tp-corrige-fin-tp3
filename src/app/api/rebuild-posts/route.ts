4cd74506355083ce13aca05a2fe46b51d50739a8
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2024-02-16 22:13:16 +0100

    question 3.7

--- src/app/api/rebuild-posts/route.ts
+++ src/app/api/rebuild-posts/route.ts
@@ -1,16 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import {generateStaticParams} from "../../[categorySlug]/[productSlug]/page";
+import {revalidatePath} from "next/cache";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const supabaseWebhookKey = process.env.SUPABASE_WEBHOOK_KEY;
     const apiKey = req.headers['api-key'];
 
     if (!apiKey) {
-        return res.status(401).json({ error: '"API-Key" header is missing' });
+        return res.status(401).json({error: '"API-Key" header is missing'});
     }
 
     if (apiKey !== supabaseWebhookKey) {
-        return res.status(403).json({ error: 'API Key is not valid' });
+        return res.status(403).json({error: 'API Key is not valid'});
+    }
+    console.log("rebuilding posts...");
+
+    const products = await generateStaticParams();
+
+    for (const product of products) {
+        revalidatePath(`/${product.categorySlug}/${product.productSlug}`);
     }
     console.log(req.headers);
     return res.status(200).json({
